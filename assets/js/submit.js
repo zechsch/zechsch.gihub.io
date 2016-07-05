d329ec8a144a1223ec7a598dba34e82e497a1aae
Zechariah Schneider <zechsch@umich.edu>
2016-07-05 10:04:53 -0400

    trying some funky shit with js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,8 +1,13 @@
 function FormSubmit()
    {
-       submitForm = document.getElementById("contactForm");
-       submitForm.action = "https://formspree.io/zechsch@umich.edu";
-       submitForm.submit();
-       submitForm.reset();
+       form = document.getElementById("contactForm");
+       header = document.getElementById("contactHeader");
+       paragraph = document.getElementById("contactP");
+       form.action = "https://formspree.io/zechsch@umich.edu";
+       form.submit();
+       form.reset();
+       header.innerHTML = "Thanks!"
+       paragraph.innerHTML = "I'll get back to you shortly."
+       form.parentNode.removeChild(form);
 	  return true;
   };
