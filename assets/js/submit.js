40c1c45fa266166c4c0d3902725101ea5ae25713
Zechariah Schneider <zechsch@umich.edu>
2016-07-05 10:13:33 -0400

    think I got the form working, again

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -7,9 +7,9 @@ function FormSubmit()
        form.action = "https://formspree.io/zechsch@umich.edu";
        form.submit();
        form.reset();
-       
+
        header.innerHTML = "Thanks!"
        paragraph.innerHTML = "I'll get back to you shortly."
        form.parentNode.removeChild(form);
-	  return true;
+	  return false;
   };
