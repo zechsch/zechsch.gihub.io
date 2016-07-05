57bafdb5328188091f124cd6b750a058914ec71e
Zechariah Schneider <zechsch@umich.edu>
2016-07-05 09:50:20 -0400

    trying some funky shit with js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,7 +1,7 @@
 function FormSubmit()
    {
-	  document.getElementbyID("contactForm").action = "https://formspree.io/zechsch@umich.edu";
+	  document.getElementbyId("contactForm").action = "https://formspree.io/zechsch@umich.edu";
 	  document.referralForm.submit();
-	  document.getElementbyID("contactForm").reset();
+	  document.getElementbyId("contactForm").reset();
 	  return true;
   };
