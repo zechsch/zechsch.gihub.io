e608d6993efbb5482f18f38137457c500cf2044d
Zechariah Schneider <zechsch@umich.edu>
2016-07-05 09:43:33 -0400

    trying some funky shit with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,14 +62,3 @@
 	});
 
 })(jQuery);
-
-$(document).ready(function()
-{
-   function FormSubmit()
-   {
-	  document.getElementbyID("contactForm").action = "https://formspree.io/zechsch@umich.edu";
-	  document.referralForm.submit();
-	  document.getElementbyID("contactForm").reset();
-	  return true;
-   }
- });
---
+++ assets/js/submit.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+   function FormSubmit()
+   {
+	  document.getElementbyID("contactForm").action = "https://formspree.io/zechsch@umich.edu";
+	  document.referralForm.submit();
+	  document.getElementbyID("contactForm").reset();
+	  return true;
+   }
+ });
